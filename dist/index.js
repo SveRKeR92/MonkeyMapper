e8e27c08dfd64c31fdfc042ea0bb433fb0ad1a81
Sverker <maxime.attala@gmail.com>
2024-01-12 09:26:24 +0100

    update index.ts : remove dead code + clean code

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.monkeyMap = exports.helloGuys = void 0;
-function helloGuys() {
-    return "Hello Guys";
-}
-exports.helloGuys = helloGuys;
+exports.monkeyMap = void 0;
 var Person = /** @class */ (function () {
     function Person(firstname, lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
         this.firstname = firstname;
         this.lastname = lastname;
     }
@@ -20,43 +14,15 @@ function createInstance(classToInstantiate) {
 function monkeyMap(entity, data) {
     // const known_fields: string[] = ["firstname", "id", "test"]
     var updated_entity = createInstance(entity);
-    // known_fields.forEach((field: string) => {
-    //     if (field in updated_entity) {
-    //         updated_entity[field] = data[field];
-    //     }})
     Object.keys(updated_entity).forEach(function (key) {
         updated_entity[key] = data[key];
     });
-    console.log(updated_entity);
     return updated_entity;
 }
 exports.monkeyMap = monkeyMap;
-monkeyMap(Person, { "firstname": "pipi", "lastname": "vert" });
-// const person = createInstance<Person>(Person);
-// const user = createInstance(User)
-// console.log(person.name); // Output: undefined
-//
-//
-// let response = {
-//     id: 5,
-//     name: "Sverker",
-//     email: "sverkerlechad@gmail.com",
-//     phone: "06123456748",
-//     address: {
-//         street: "iii",
-//         street_number: "1212"
-//     }
-// }
-/*
-let mappedData = User(
-    id = 5,
-    name = "Sverker",
-    email = "sverkerlechad@gmail.com"
-)*/
-//
-// const create_class_from_name = (class_name: string): any => {
-//     return new Function('return new ' + class_name)()
-// }
-//
-// const person1 = create_class_from_name("Person")
-// console.log("Person 1 should be created : ", person1)
+var dummy_response_from_api = {
+    "firstname": "DummyFirstname",
+    "lastname": "DummyLastname",
+};
+var user_exemple = monkeyMap(Person, dummy_response_from_api);
+console.log(user_exemple);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-export function helloGuys(): string {
-    return "Hello Guys"
-}
-
 class Person{
-    constructor(public firstname: string, public lastname: string) {
-        this.firstname = firstname
-        this.lastname = lastname
-    }
+    constructor(
+        public firstname: string,
+        public lastname: string,
+    ) {}
 }
 
 function createInstance<T>(classToInstantiate: new () => T): T {
@@ -16,51 +12,18 @@ function createInstance<T>(classToInstantiate: new () => T): T {
 export function monkeyMap(entity: any, data: {[key: string]: string | any}){
     // const known_fields: string[] = ["firstname", "id", "test"]
     const updated_entity: any = createInstance(entity)
-    // known_fields.forEach((field: string) => {
-    //     if (field in updated_entity) {
-    //         updated_entity[field] = data[field];
-    //     }})
+
     Object.keys(updated_entity).forEach(key => {
         updated_entity[key] = data[key]
     })
-    console.log(updated_entity)
     return updated_entity
 }
 
-monkeyMap(Person, {"firstname": "pipi", "lastname": "vert"})
+const dummy_response_from_api = {
+    "firstname": "DummyFirstname",
+    "lastname": "DummyLastname",
+}
 
-// const person = createInstance<Person>(Person);
-// const user = createInstance(User)
-// console.log(person.name); // Output: undefined
+const user_exemple = monkeyMap(Person, dummy_response_from_api)
 
-
-//
-//
-// let response = {
-//     id: 5,
-//     name: "Sverker",
-//     email: "sverkerlechad@gmail.com",
-//     phone: "06123456748",
-//     address: {
-//         street: "iii",
-//         street_number: "1212"
-//     }
-// }
-
-
-
-/*
-let mappedData = User(
-    id = 5,
-    name = "Sverker",
-    email = "sverkerlechad@gmail.com"
-)*/
-
-//
-// const create_class_from_name = (class_name: string): any => {
-//     return new Function('return new ' + class_name)()
-// }
-//
-// const person1 = create_class_from_name("Person")
-// console.log("Person 1 should be created : ", person1)
-
+console.log(user_exemple)
